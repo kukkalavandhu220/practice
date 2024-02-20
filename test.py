57846fa967ca9fc6b4111afbab39fe44ef460013
kukkalavandhu220 <kukkalavandana@gmail.com>
2024-02-20 12:52:49 +0530

    first commit

---
+++ test.py
@@ -0,0 +1,88 @@
+about="STUDENT DETAILS"
+print(about)
+name="vandana"
+age=21
+rollno=520
+fatherName="yesu ratnam"
+aadhar= 844719197771
+emailid=",vandana2002@gmail.com"
+mobileNo=9392755070
+address="5-104,tilak street,kakinada,kakinada district,Andhra Pradesh"
+grade="1st"
+percentage=98.5
+print("my name is",name,"i have",age,"years old and my rollno is",\
+      rollno,"my father name is",fatherName,"my aadhar and emailid`s are",\
+      aadhar,emailid,"and my mobile number is",mobileNo,"and my grade with \
+percentage is",grade,"&",percentage)
+
+space="     "
+print(space)
+about="TRAIN PASSENGER"
+print(about)
+name="simha"
+age=21
+destination="kkd to rjy"
+gender="male"
+nationality="INDIAN"
+mobileno=9052336025
+emailid="simha@gmail.com"
+address="2-165,pamidipadu,narasaraopet mandal,guntur district,Andhra Pradesh"
+trainName="seshadri exp"
+trainNo=34324
+bookingStatus="CNF/B3/26 LB"
+ticketFare=200
+print(name," is a train passenger.His gender is",gender,".He has ",age,\
+      "years old.He has to travel from",destination,"he is an",nationality,"his mobile number and",\
+      "emailid is",mobileno,",",emailid,"and his address is",address,"he has to travel on",\
+      trainName,"train munber",trainNo,"his booking status is",bookingStatus,"and ticket",\
+      "fare is",ticketFare)
+
+space="       "
+print(space)
+about="SOFTWARE EMPLOYEE"
+print(about)
+name="honey"
+age="25 Years." 
+employeeid=134
+position="developer"
+salary=50000
+companyName="Aadiguru Prodigy"
+companyAddress="2nd floor 18th B Main,9th Cross Rd,Marenahalli,2nd phase,j.p.nagar,bengaluru"
+emailid="honey345@gmail.com"
+mobileNo=9876543210
+nationality="INDIAN"
+employeeAddress="5-104,tilak street,kakinada,kakinada district,Andhra Pradesh"
+print("An",companyName,"is a software company,which is located at ",companyAddress,\
+      ".IN this company",name,"is an employee she has",age,"she is a",position,"with",\
+      salary,"package and her id is",employeeid,"she is an",nationality,"and her",\
+      "mobile number and email id is",mobileNo,",",emailid)
+      
+
+space="   "
+print(space)
+about="BANK ACCOUNT HOLDER DETAILS"
+print(about)
+AccountholderName="pinky"
+fatherName="sasi"
+gender="female"
+aadharNo=345609871234
+emailid="pinky43455@gmail.com"
+mobileNo=6345674329
+branchName="SBI KAKINADA"
+accountType="personal"
+DOB="2ND AUG 2002"
+address="bhanugudi center,kakinada,kakinada district,Andhra Pradesh"
+print("In",branchName,"branch",AccountholderName,"is opened a",accountType,\
+      "account.his gender","is",gender,"and",fatherName,"is her father.In account",\
+      "opening time",AccountholderName,"gave some personal details like aadharNo:",\
+      aadharNo,",mobileNo:",mobileNo,",emailid:",emailid,",date of birth:",DOB,\
+      "and her address:",address,)
+
+
+ 
+
+
+
+
+
+
